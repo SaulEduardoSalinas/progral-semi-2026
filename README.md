f021d34a21f7a9a2c74898a3904f2cb3c160c71d
Saul Eduardo Salinas <pikachupokemos43@gmail.com>
2026-09-08 12:27:34 -0600

    Subida exclusiva de Parcial uno y Parcial uno conversor

--- README.md
+++
@@ -1,2 +0,0 @@
-# progral-semi-2026
-clases código de la catedra de programación computacional1 
--- laboratorio de programacion  1/laboratorio de programacion  1/fmrmaestro.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class fmrmaestro
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Close()
-    End Sub
-End Class
