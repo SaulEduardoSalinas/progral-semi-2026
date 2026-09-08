9ad17ea93071f6d1929ae5cfa36896d12b3d387f
Saul Eduardo Salinas <pikachupokemos43@gmail.com>
2026-09-08 12:01:54 -0600

    Correcion de errores

---
+++ formula de captura de datos/formula de captura de datos/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim nombre As String
+        Dim apellido As String
+        Dim edad As String
+        Dim transporte As String = ""
+        Dim sexo As String = ""
+
+        nombre = TextBox1.Text
+        apellido = TextBox2.Text
+        edad = TextBox3.Text
+
+        If CheckBox1.Checked Then
+            transporte = transporte & "Carro "
+        End If
+
+        If CheckBox2.Checked Then
+            transporte = transporte & "Moto "
+        End If
+
+        If CheckBox3.Checked Then
+            transporte = transporte & TextBox4.Text
+        End If
+
+        If RadioButton1.Checked Then
+            sexo = "Masculino"
+        End If
+
+        If RadioButton2.Checked Then
+            sexo = "Femenino"
+        End If
+
+        MessageBox.Show("Nombre: " & nombre &
+                        vbCrLf & "Apellido: " & apellido &
+                        vbCrLf & "Edad: " & edad &
+                        vbCrLf & "Transporte: " & transporte &
+                        vbCrLf & "Sexo: " & sexo)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        End
+
+    End Sub
+
+    Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+
+
+        If CheckBox3.Checked Then
+            Label5.Visible = True
+            TextBox4.Visible = True
+        Else
+            Label11.Visible = False
+            TextBox4.Visible = False
+        End If
+
+    End Sub
---
+++ laboratorio de programacion  1/laboratorio de programacion  1/fmrestudiantes.vb
@@ -0,0 +1,13 @@
+﻿Public Class fmrestudiantes
+    Private Sub fmrestudiantes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
